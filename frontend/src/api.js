4e2d98a70e5a19b1d35162b941f8ab13c7bec551
Muhammad Faizan <muhammadfaizanlite@gmail.com>
2025-01-23 16:55:23 +0500

    Modify the api.js with the backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "/choreo-apis/reactdjangonotes-lo/backend/v1";
 
 const api = axios.create(
     {

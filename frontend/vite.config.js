3817901bbfc19ccd38ca92c50139d97d574dcaf6
Muhammad Faizan <muhammadfaizanlite@gmail.com>
2025-01-23 15:39:38 +0500

    Modify the vite.config.js and package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'], 
-  },
   build: {
     outDir: 'dist', 
-    sourcemap: true, 
+    emptyOutDir: true, // Clears the output directory before each build
   },
-  server: {
-    port: 3000, 
-    open: true, 
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
 });
